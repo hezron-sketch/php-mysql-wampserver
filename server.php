64e46e4c655a138a00864ba3b8085cc2e9ccdf80
hezron-sketch <hpaulhezne@gmail.com>
2024-04-12 02:58:32 -0700

    FInal

--- server.php
+++ server.php
@@ -21,8 +21,7 @@ if (isset($_POST['reg_user'])) {
     $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
 
-    // form validation: ensure that the form is correctly filled ...
-    // by adding (array_push()) corresponding error unto $errors array
+
     if (empty($username)) {
         array_push($errors, "Username is required");
     }
@@ -36,8 +35,7 @@ if (isset($_POST['reg_user'])) {
         array_push($errors, "The two passwords do not match");
     }
 
-    // first check the database to make sure 
-    // a user does not already exist with the same username and/or email
+
     $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
     $result = mysqli_query($db, $user_check_query);
     $user = mysqli_fetch_assoc($result);
@@ -56,8 +54,8 @@ if (isset($_POST['reg_user'])) {
     if (count($errors) == 0) {
         $password = md5($password_1);//encrypt the password before saving in the database
 
-        $query = "INSERT INTO users (username, email, password) 
-                          VALUES('$username', '$email', '$password')";
+        $query = "INSERT INTO users (username,name, phone, email, password) 
+                          VALUES('$username','$name', '$phone', '$email', '$password')";
         mysqli_query($db, $query);
         $_SESSION['username'] = $username;
         $_SESSION['success'] = "You are now logged in";
